ff17401268202a0fdbcd066981c9180d0c96e588
st-slav <st.mam_@hotmail.com>
2016-02-27 15:44:18 +0500

    first commit

---
+++ src/main/java/com/test/rs/util/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.test.rs.util;
+
+import com.test.rs.rsevice.LocationRest;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("rs")
+public class ApplicationConfig extends Application{
+    
+    private final Set<Class<?>> classes;
+    
+    public ApplicationConfig() {
+        HashSet<Class<?>> c = new HashSet<>();
+        c.add(LocationRest.class);
+
+        classes = Collections.unmodifiableSet(c);
+    }
+    
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+
+}
